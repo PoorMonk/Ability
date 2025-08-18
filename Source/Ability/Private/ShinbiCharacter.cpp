acfd381fbc495729a1d7ff1a44c198089c05e70d
贺济 <heji@cestc.cn>
2025-08-18 11:29:26 +0800

    feat: 🎸 添加人物的行走跳跃以及鼠标的转向

---
+++ Source/Ability/Private/ShinbiCharacter.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShinbiCharacter.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+// Sets default values
+AShinbiCharacter::AShinbiCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArmComponent->SetupAttachment(GetRootComponent());
+	SpringArmComponent->TargetArmLength = 300;
+	SpringArmComponent->SocketOffset = FVector(0, 0, 60);	// 
+	SpringArmComponent->bEnableCameraLag = true;
+	SpringArmComponent->bUsePawnControlRotation = true;
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
+	CameraComponent->bUsePawnControlRotation = false;
+}
+
+// Called when the game starts or when spawned
+void AShinbiCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShinbiCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AShinbiCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+
+	if (Controller != nullptr)
+	{
+		
+	}
+}
+
---
+++ Source/Ability/Public/ShinbiGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShinbiGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABILITY_API AShinbiGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AShinbiGameMode();
+};
